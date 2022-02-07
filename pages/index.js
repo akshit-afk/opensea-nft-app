641052d4276b01b645a7c654c0fc1f2c5a5b23b5
akshit-afk <akshit1200.cse19@chitkara.edu.in>
2022-02-07 18:23:39 +0530

    Connected database

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Header from '../components/header'
 import Hero from '../components/hero'
 import { useWeb3 } from '@3rdweb/hooks'
+import { useEffect } from 'react'
+import { client } from '../lib/sanityClient'
+import toast, { Toaster } from 'react-hot-toast'
 
 const style = {
   wrapper: ``,
@@ -11,8 +14,36 @@ const style = {
 }
 export default function Home() {
   const { address, connectWallet } = useWeb3()
+  const welcomeUser = (userName, toastHandler = toast) => {
+    toastHandler.success(
+      `Welcome back ${userName !== 'Unnamed' ? `${userName}` : ''}!`,
+      {
+        style: {
+          background: '#04111d',
+          color: '#fff',
+        },
+      }
+    )
+  }
+  useEffect(() => {
+    if (!address)
+      return /* here i am using IIFE(immidiately invoked functionalexpression*/
+      /* basicallyy it just calls the function automatically */
+      /* syntax: (function(){})();*/
+    ;(async () => {
+      const userDoc = {
+        _type: 'users',
+        _id: address,
+        userName: 'Unnamed',
+        walletAddress: address,
+      }
+      const result = await client.createIfNotExists(userDoc)
+      welcomeUser(result.userName)
+    })()
+  }, [address])
   return (
     <div className={style.wrapper}>
+      <Toaster position="bottom-left" reverseOrder={false} />
       {address ? (
         <>
           <Header />
